bdc9470bf4d5da4d5ca0929af156f64a011645df
cbryanalfonso <cbryanalfonso@gmail.com>
2022-03-02 15:46:07 -0600

    Tabla de pedidos

--- js/pedido.js
+++ js/pedido.js
@@ -1,12 +1,52 @@
-function guardar(){
-   
+function guardar() {
+
     var _nom = document.getElementById("Nombre").value;
     var _tel = document.getElementById("Telefono").value;
     var _emai = document.getElementById("Email").value;
+    var _pastel = document.getElementById("pastel").value;
+    var checks = document.querySelectorAll(".valores");
+    var boton = document.getElementById("btn_guardar");
+    var superHeroe = document.getElementById('superherofav').value;
+    var mensajeCumple = document.getElementById('mensajeCumple').value;
 
-    var fila="<tr><td>"+_nom+"</td><td>"+_tel +"</td><td>"+_emai +"</td></tr>";
+    ///todos = new Array()
+    var todo = []
+    var estado = false
+    boton.addEventListener('click', function () {
+        checks.forEach((e) => {
+            console.log(e.checked);
+            if (e.checked === true) {
+                //console.log(e.value);
+                //todo.push(e.value)
+                if (e.value === 'SuperHeroe Favorito'){
+                    estado = true
+                    todo.push(e.value)
+                }else{
+                    todo.push(e.value)
+                }
+                
+            }
+        });
+       // console.log(todo.length);
+        superHeroe ? todo.push(superHeroe) : console.log("Nada")
+        mensajeCumple ? todo.push(" Mensaje de cumpleaños "+ mensajeCumple): console.log("naa")
+       
+        var fila = "<tr><td>" + _nom + "</td><td>" + _tel + "</td><td>" + _emai + "</td><td>" + _pastel + "</td><td>" +todo.join(', ')  + "</td></tr>";
 
-    var btn = document.createElement("TR");
-   	btn.innerHTML=fila;
-    document.getElementById("tablita").appendChild(btn);
+        var btn = document.createElement("TR");
+        btn.innerHTML = fila;
+        document.getElementById("tablita").appendChild(btn);
+        todo=[]
+    })
+
+
+
+    /* todos = new Array();
+    for (var i = 0, total = f[cual].length; i < total; i++)
+        if (f[cual][i].checked) todos[todos.length] = f[cual][i].value;
+    //console.log(todos.join(", ")); */
+
+
+
+
 }
